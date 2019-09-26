379319e325672f6107b28f3f242ea13d7f03b1fd
Igor Abreu <igor.abreu.reis@gmail.com>
2019-09-26 10:40:50 -0300

    fix: category count

--- src/components/layout.js
+++ src/components/layout.js
@@ -16,18 +16,19 @@ class Layout extends Component {
     this.state = {
       mapInfo: {
         date: 20181019,
-        picture: '20181019_1115_966175624',
-        description: 'Problemas de alagamentos sempre que chove',
-        category: 'Alagamento',
-        frequency: 'Ocasionalmente',
-        address: 'Rua Santa Rita,',
-        location: 'Nova Holanda',
-        lat: -22.853554,
-        long: -43.244659,
-        triedToSolve: 'nao menciona',
-        externalHelp: 'nao se aplica',
-        totalAmount: 25,
-        categoryAmount: 2,
+        picture: '20181019_0033_972730791',
+        description:
+          'Oi olha só aqui no esgoto da casa 206! Tá um fedor insuportável! Já fiz reclamções na Cedae, e eles disseram que este esgoto precisa de fazer obra e é que isso não é da alçada deles , e sim do dono. Toda vez esse esgoto fica vazando.',
+        category: 'Esgoto a Céu Aberto',
+        frequency: 'Frequentemente',
+        address: 'Rua Massaranduba, 206',
+        location: 'Parque Rubens Vaz',
+        lat: -22.852344,
+        long: -43.2429,
+        triedToSolve: 'sim',
+        externalHelp: '-',
+        totalAmount: 39,
+        categoryAmount: 5,
       },
     }
   }
@@ -35,8 +36,9 @@ class Layout extends Component {
   handleMapInfo(info) {
     const totalAmount = dataMarkers.length
     const categoryItems = dataMarkers.filter(item => {
-      return item.category === info.category
+      return item.category.toUpperCase() === info.category.toUpperCase()
     })
+    console.log(categoryItems)
     const categoryAmount = categoryItems.length
 
     this.setState({
